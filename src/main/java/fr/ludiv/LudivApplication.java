52d6cec2eeff04769addbb136c32d9d1f102b21e
GABA-MANO <ybgabamano@gmail.com>
2022-02-15 09:29:50 +0100

    Init project

---
+++ src/main/java/fr/ludiv/LudivApplication.java
@@ -0,0 +1,13 @@
+package fr.ludiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ludiv/LudivApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ludiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
